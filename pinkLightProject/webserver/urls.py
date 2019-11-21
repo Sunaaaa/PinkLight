7e049e4c9799514b4e9f23a24e40049c55906bb3
Sunah Gong <glglthssla@naver.com>
2019-11-21 21:59:55 +0900

    지하철 실시간 도착정보 가져오기 완료 | train_no 별 group_by 진행 ing ( 총 count 는 함  )

--- pinkLightProject/webserver/urls.py
+++ pinkLightProject/webserver/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib import admin
 from . import views
 
 app_name = 'webserver'
@@ -7,4 +8,7 @@ urlpatterns = [
     path('new/', views.new, name="new"),
     path('<int:train_pk>/', views.detail, name="detail"),
     path('<int:train_pk>/edit/', views.edit, name="edit"),
+    path('<int:train_pk>/delete/', views.delete, name="delete"),
+    path('<str:train_name>/station_info/', views.station_info, name="station_info"),
+    path('<str:station>/station_status', views.station_status, name="station_status"),
 ]
--- pinkLightProject/webserver/views.py
+++ pinkLightProject/webserver/views.py
@@ -2,11 +2,204 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Train
 from django import forms
 from .forms import TrainForm
+from django.http import JsonResponse
+from django.db.models import Count
+import requests
 
+def station_status(request, station):
+    print('뿜뿜뿜')
+    print(type(station))
+    # station_status = request.get(f'http://swopenapi.seoul.go.kr/api/subway/674d6171516d696e3432724143445a/json/realtimeStationArrival/0/5/{station}').json
+    station_status = requests.get(f'http://swopenapi.seoul.go.kr/api/subway/674d6171516d696e3432724143445a/json/realtimeStationArrival/0/5/{station}').json()
+    print(type(station_status))
+    # print(station_status[realtimeArrivalList])
+    data = station_status['realtimeArrivalList']
+    print(len(data))
+    context = {
+        'one' : data[0],
+        'two' : data[1],
+    }
+    return JsonResponse(context)
+
+
+def station_info(request, train_name):
+    print(train_name)
+    context = {
+        '상행' : [
+            {
+                '001002': [{
+                    'slot_no' : '004',
+                    'seat_no' : '01',
+                    'empty' : False
+                }, {
+                    'slot_no' : '004',
+                    'seat_no' : '02',                    
+                    'empty' : False
+                }, {
+                    'slot_no' : '004',
+                    'seat_no' : '03',                    
+                    'empty' : False
+                }, {
+                    'slot_no' : '005',
+                    'seat_no' : '01',                    
+                    'empty' : False
+                }, {
+                    'slot_no' : '005',
+                    'seat_no' : '02',                    
+                    'empty' : False
+                }, {
+                    'slot_no' : '005',
+                    'seat_no' : '03',                    
+                    'empty' : True
+                }, {
+                    'slot_no' : '006',
+                    'seat_no' : '01',                    
+                    'empty' : False
+                }, {
+                    'slot_no' : '006',
+                    'seat_no' : '02',                    
+                    'empty' : False
+                }, {
+                    'slot_no' : '006',
+                    'seat_no' : '03',                    
+                    'empty' : True
+                }
+                ]
+            },
+            {
+                '001003': [{
+                    'slot_no' : '004',
+                    'seat_no' : '01',
+                    'empty' : True
+                }, {
+                    'slot_no' : '004',
+                    'seat_no' : '02',
+                    'empty' : True
+                }, {
+                    'slot_no' : '005',
+                    'seat_no' : '01',                    
+                    'empty' : True
+                }, {
+                    'slot_no' : '005',
+                    'seat_no' : '01',                    
+                    'empty' : True
+                }, {
+                    'slot_no' : '005',
+                    'seat_no' : '02',                    
+                    'empty' : True
+                }, {
+                    'slot_no' : '005',
+                    'seat_no' : '03',                    
+                    'empty' : True
+                }, {
+                    'slot_no' : '006',
+                    'seat_no' : '01',                    
+                    'empty' : False
+                }, {
+                    'slot_no' : '006',
+                    'seat_no' : '02',                    
+                    'empty' : False
+                }, {
+                    'slot_no' : '006',
+                    'seat_no' : '03',                    
+                    'empty' : False
+                }
+                ]
+            }
+        ],
+        '하행' : [
+            {
+                '001004': [{
+                    'slot_no' : '004',
+                    'seat_no' : '01',
+                    'empty' : False
+                }, {
+                    'slot_no' : '004',
+                    'seat_no' : '02',                    
+                    'empty' : True
+                }, {
+                    'slot_no' : '004',
+                    'seat_no' : '03',                    
+                    'empty' : True
+                }, {
+                    'slot_no' : '005',
+                    'seat_no' : '01',                    
+                    'empty' : True
+                }, {
+                    'slot_no' : '005',
+                    'seat_no' : '02',                    
+                    'empty' : True
+                }, {
+                    'slot_no' : '005',
+                    'seat_no' : '03',                    
+                    'empty' : False
+                }, {
+                    'slot_no' : '006',
+                    'seat_no' : '01',                    
+                    'empty' : True
+                }, {
+                    'slot_no' : '006',
+                    'seat_no' : '02',                    
+                    'empty' : True
+                }, {
+                    'slot_no' : '006',
+                    'seat_no' : '03',                    
+                    'empty' : True
+                }
+                ]
+            },
+            {
+                '001005': [{
+                    'slot_no' : '004',
+                    'seat_no' : '01',
+                    'empty' : False
+                }, {
+                    'slot_no' : '004',
+                    'seat_no' : '02',
+                    'empty' : False
+                }, {
+                    'slot_no' : '004',
+                    'seat_no' : '03',                    
+                    'empty' : True
+                }, {
+                    'slot_no' : '005',
+                    'seat_no' : '01',                    
+                    'empty' : False
+                }, {
+                    'slot_no' : '005',
+                    'seat_no' : '02',                    
+                    'empty' : False
+                }, {
+                    'slot_no' : '005',
+                    'seat_no' : '03',                    
+                    'empty' : True
+                }, {
+                    'slot_no' : '006',
+                    'seat_no' : '01',                    
+                    'empty' : True
+                }, {
+                    'slot_no' : '006',
+                    'seat_no' : '02',                    
+                    'empty' : True
+                }, {
+                    'slot_no' : '006',
+                    'seat_no' : '03',                    
+                    'empty' : True
+                
+                }]
+            }
+        ],
+
+    }
+    return JsonResponse(context)
+    
 
 # Create your views here.
 def index(request):
     trains = Train.objects.all()
+    tt = Train.objects.values('train_no').annotate(total=Count('train_no'))
+    print(tt)
+
     context = {
         'trains' : trains,
     }
@@ -51,7 +244,7 @@ def edit(request, train_pk):
         print("뿜뿜")
         if form.is_valid():
             train = form.save()
-            return redirect('webserver:index', train_pk)
+            return redirect('webserver:detail', train_pk)
         
     else:
         form = TrainForm(instance=train)
@@ -61,3 +254,6 @@ def edit(request, train_pk):
     }
 
     return render(request, 'webserver/form.html', context)
+
+def delete(request):
+    pass
